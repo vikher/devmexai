92c71e12ca4ee51212b39d2f6228d07f5aea8264
victor hernandez <v.hernandez.s12373@gmail.com>
2021-01-23 14:34:28 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-angular-site';
+  myimage = 'assets/images/softdev.png';
+  app1 = 'assets/images/app1.jpg';
+  app2 = 'assets/images/app2.jpg';
+  app3 = 'assets/images/app3.jpg';
+  appdesign = 'assets/images/appdesign.jpg';
+  qacontrol = 'assets/images/qacontrol.jpg';
+  dev = 'assets/images/dev.jpg';
+  wireframe = 'assets/images/wireframe.jpg';
+  req = 'assets/images/req.jpg';
+  foodapp = 'assets/images/foodapp.png';
+  freechat = 'assets/images/freechat.gif';
+  flight = 'assets/images/flight.gif';
+  games = 'assets/images/games.gif';
+  delivery = 'assets/images/delivery.gif';
+  ecommerce = 'assets/images/ecommerce.gif';
+  nomadic = 'assets/images/nomadic.gif';
+  checkout = 'assets/images/checkout.png';
+  cloudmusic = 'assets/images/cloudmusic.gif';
+  logo = 'assets/images/logo.png';
+
+
+}
